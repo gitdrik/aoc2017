6d711cb74cf7c5dca2426d7ba00b03489fecaf3f
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-05-10 12:35:57 +0200

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,17 @@
+import copy
+with open('06.txt') as f:
+    banks = [int(x) for x in f.read().strip().split()]
+cn = {}
+i = 0
+while tuple(banks) not in cn:
+    cn[tuple(banks)] = i
+    blocks = max(banks)
+    index = banks.index(blocks)
+    banks[index] = 0
+    while blocks:
+        index = (index+1)%16
+        blocks -= 1
+        banks[index] += 1
+    i += 1
+print('Part 1: ',i)
+print('Part 2: ',i-cn[tuple(banks)])
